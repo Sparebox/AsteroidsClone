226f2fc77b4a460b5df6892c9de8f67a2ed9d13f
Oskari Ojamaa <71134757+Sparebox@users.noreply.github.com>
2022-01-01 20:08:07 +0200

    Added menu music

--- core/src/gdx/asteroidsclone/screens/MenuScreen.java
+++ core/src/gdx/asteroidsclone/screens/MenuScreen.java
@@ -3,9 +3,13 @@ package gdx.asteroidsclone.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL30;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
@@ -17,13 +21,19 @@ import gdx.asteroidsclone.Main;
 public class MenuScreen extends ScreenAdapter {
 
     public static final int BUTTON_WIDTH = 100;
+    public static final int FONT_SIZE = 50;
 
     private Stage stage;
     private Table table;
+    private BitmapFont font;
+    private FreeTypeFontGenerator fontGenerator = Main.INSTANCE.fontGenerator;
+    private FreeTypeFontGenerator.FreeTypeFontParameter fontParameter = Main.INSTANCE.fontParameter;
+    private Label titleLabel;
     private TextButton startButton;
     private TextButton optionsButton;
     private TextButton exitButton;
     private Skin skin = Main.INSTANCE.skin;
+    private Music menuTheme;
 
     public MenuScreen() {
         stage = new Stage(new StretchViewport(Main.INSTANCE.GUI_WIDTH, Main.INSTANCE.GUI_HEIGHT));
@@ -32,6 +42,11 @@ public class MenuScreen extends ScreenAdapter {
         table.setFillParent(true);
         table.setDebug(false);
         stage.addActor(table);
+        fontParameter.size = FONT_SIZE;
+        font = fontGenerator.generateFont(fontParameter);
+        var style = new Label.LabelStyle();
+        style.font = font;
+        titleLabel = new Label("Asteroids Clone", style);
         startButton = new TextButton("Start", skin);
         startButton.addListener(new ClickListener(){
             @Override
@@ -53,11 +68,17 @@ public class MenuScreen extends ScreenAdapter {
                 Gdx.app.exit();
             }
         });
+        table.add(titleLabel);
+        table.row().pad(100,0,10,0);
         table.add(startButton).width(BUTTON_WIDTH);
-        table.row().pad(10,0,10,0);
+        table.row().pad(0,0,10,0);
         table.add(optionsButton).width(BUTTON_WIDTH);
-        table.row();
+        table.row().pad(0,0,10,0);
         table.add(exitButton).width(BUTTON_WIDTH);
+        menuTheme = Gdx.audio.newMusic(Gdx.files.internal("sounds/asteroids_clone_theme.wav"));
+        menuTheme.play();
+        menuTheme.setVolume(0.5f);
+        menuTheme.setLooping(true);
     }
 
     @Override
@@ -77,6 +98,7 @@ public class MenuScreen extends ScreenAdapter {
     @Override
     public void dispose() {
         stage.dispose();
+        menuTheme.dispose();
     }
 
 }
